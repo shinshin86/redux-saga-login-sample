dfc62dcc3b7dc3a96c66b366269dcc27240cabfd
shinshin86 <beagles1986@gmail.com>
2018-04-01 08:46:34 +0900

    Add control of login and redirect.

---
+++ src/containers/Auth.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types' 
+import { connect } from 'react-redux'
+import { Route, Redirect, withRouter } from 'react-router-dom'
+
+const Auth = ({ loggedIn, children }) => {
+    return (
+      loggedIn? (
+        <Route children={children} />
+      ) : (
+        <Redirect to={'/'} />
+      )
+    )
+}
+
+const mapStateToProps = (state) => ({
+  loggedIn: state.login.loggedIn
+})
+
+export default withRouter(connect(mapStateToProps)(Auth))
--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import Dashboard from './containers/Dashboard'
+import Auth from './containers/Auth'
 import configureStore from './store'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory'
 
@@ -16,8 +17,14 @@ render(
   <Provider store={store}>
     <ConnectedRouter history={history} >
       <div>
-        <Route exact path="/" component={App} />
-        <Route path="/dashboard" component={Dashboard} />
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Auth>
+            <Switch>
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </Auth>
+        </Switch>
       </div>
     </ConnectedRouter>
   </Provider>,
