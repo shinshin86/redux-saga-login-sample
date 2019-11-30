2cc8b7e2de77c5d89c276f35cf9f7ca975dfa270
shinshin86 <beagles1986@gmail.com>
2019-11-30 22:30:42 +0900

    format webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,28 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'src', 'main'),
+    path.join(__dirname, 'src', 'main')
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/',
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
   ],
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: [ 'babel-loader' ],
-      exclude: /node_modules/,
-      include: __dirname,
-    }],
-  },
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
 }
